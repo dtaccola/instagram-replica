a347c2415998285c86e4eca46121f1efcab935e5
dtaccola <danieltaccola@yahoo.com.br>
2022-06-30 19:47:48 -0300

    componente post

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import './App.css';
 import {auth, db, storage, functions} from './firebase.js';
 import {useEffect, useState} from 'react';
 import Header from './Header';
+import Post from './Post';
 
 function App() {
   const [user, setUser] = useState();
+  const [posts, setPosts] = useState([]);
 
   useEffect(()=>{
+    db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(function(snapshot){
+      setPosts(snapshot.docs.map(function(document){
+        return {id:document.id, info:document.data()}
+      }))
+    })
 
   },[])
+
+
 
   return (
     <div className="App">
       <Header setUser={setUser} user={user}></Header>
+
+      {
+        posts.map(function(val){
+          return(
+            <Post info={val.info} id={val.id} />
+            
+          )
+        })
+      }
     </div>
   );
 }
